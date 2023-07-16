fcaed2a70fb493034034df2081a721e54fc23687
Mehedi Pathan <eity.mehedipathan@gmail.com>
2023-07-16 15:16:34 -0700

    readme file added and localstore add not working

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,5 +1,5 @@
-/* eslint-disable react/prop-types */
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import './TodoApp.scss';
 
 const TodoApp = () => {
@@ -8,9 +8,21 @@ const TodoApp = () => {
   useEffect(() => {
     const storedTasks = localStorage.getItem('tasks');
     if (storedTasks) {
-      setTasks(JSON.parse(storedTasks));
+      try {
+        const parsedTasks = JSON.parse(storedTasks);
+        if (Array.isArray(parsedTasks)) {
+          setTasks(parsedTasks);
+        }
+      } catch (error) {
+        console.error('Error parsing stored tasks:', error);
+      }
     }
   }, []);
+  
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
 
   const handleAddTask = (task) => {
     const updatedTasks = [...tasks, task];
@@ -24,15 +36,29 @@ const TodoApp = () => {
     localStorage.setItem('tasks', JSON.stringify(updatedTasks));
   };
 
+  const handleToggleTaskStatus = (taskId) => {
+    const updatedTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return {
+          ...task,
+          completed: !task.completed,
+        };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+    localStorage.setItem('tasks', JSON.stringify(updatedTasks));
+  };
+
   return (
     <div className="todo_app">
       <h1>Todo List App</h1>
       <TaskForm onAddTask={handleAddTask} />
-      {tasks.length > 0 ? (
-        <TaskList tasks={tasks} onDeleteTask={handleDeleteTask} />
-      ) : (
-        <p>No tasks assigned yet</p>
-      )}
+      <TaskList
+        tasks={tasks}
+        onDeleteTask={handleDeleteTask}
+        onToggleTaskStatus={handleToggleTaskStatus}
+      />
     </div>
   );
 };
@@ -43,7 +69,11 @@ const TaskForm = ({ onAddTask }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (task.trim()) {
-      const newTask = { id: Date.now(), description: task };
+      const newTask = {
+        id: Date.now(),
+        description: task,
+        completed: false,
+      };
       onAddTask(newTask);
       setTask('');
     }
@@ -54,39 +84,76 @@ const TaskForm = ({ onAddTask }) => {
       <input
         type="text"
         value={task}
-        className='todo_input'
         onChange={(e) => setTask(e.target.value)}
         placeholder="Enter a new task"
-        style={{ color: 'black' }}
       />
       <button type="submit">Add Task</button>
     </form>
   );
 };
 
-const TaskList = ({ tasks, onDeleteTask }) => {
+TaskForm.propTypes = {
+  onAddTask: PropTypes.func.isRequired,
+};
+
+const TaskList = ({ tasks, onDeleteTask, onToggleTaskStatus }) => {
   return (
-    <ul>
-      {tasks.map((task) => (
-        <TaskItem key={task.id} task={task} onDeleteTask={onDeleteTask} />
-      ))}
+    <ul className="task-list">
+      {tasks.length > 0 ? (
+        tasks.map((task) => (
+          <TaskItem
+            key={task.id}
+            task={task}
+            onDeleteTask={onDeleteTask}
+            onToggleTaskStatus={onToggleTaskStatus}
+          />
+        ))
+      ) : (
+        <p>No tasks assigned yet</p>
+      )}
     </ul>
   );
 };
 
-const TaskItem = ({ task, onDeleteTask }) => {
+TaskList.propTypes = {
+  tasks: PropTypes.array.isRequired,
+  onDeleteTask: PropTypes.func.isRequired,
+  onToggleTaskStatus: PropTypes.func.isRequired,
+};
+
+const TaskItem = ({ task, onDeleteTask, onToggleTaskStatus }) => {
   const handleDelete = () => {
     onDeleteTask(task.id);
   };
 
+  const handleToggleStatus = () => {
+    onToggleTaskStatus(task.id);
+  };
+
   return (
-    <li className='todo_list'>
+    <li className={`task-item ${task.completed ? 'completed' : ''}`}>
       <span>{task.description}</span>
-      <button onClick={handleDelete} className='del_btn'>
+      <button
+        className={`status-btn ${task.completed ? 'green' : 'yellow'}`}
+        onClick={handleToggleStatus}
+      >
+        {task.completed ? 'Complete' : 'Ongoing'}
+      </button>
+      <button className="del-btn" onClick={handleDelete}>
         Delete
       </button>
     </li>
   );
+};
+
+TaskItem.propTypes = {
+  task: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    description: PropTypes.string.isRequired,
+    completed: PropTypes.bool.isRequired,
+  }).isRequired,
+  onDeleteTask: PropTypes.func.isRequired,
+  onToggleTaskStatus: PropTypes.func.isRequired,
 };
 
 export default TodoApp;
